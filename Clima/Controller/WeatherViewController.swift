3afca10a56253e918f98a6e3f6306aff08ac15c1
Babiplima123 <barbara.lima@zup.com.br>
2023-04-14 00:52:22 -0300

    first-commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Bárbara 14/04/2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
